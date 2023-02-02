5a5799b129bf3782eb9785f1dbc5d4359c43a9f9
Jose Guardiola Argumedo <jguardiola9510@gmail.com>
2023-02-01 18:20:41 -0600

    feat: error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { errorHandler } from './middlewares/errorHandler';
 import routes from './routes';
 
-const PORT = 4002;
+const PORT = 80;
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.use('/api', routes);
 app.get('/', (req, res) => {
   res.json({ msg: 'TANGO CHALLENGE' });
 });
+
+// Error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Tango api-challenge listening on port ${PORT}`);
